ff897193b37292ec5c76f46db62b35082cb8e205
BlueZeeKing24 <52867955+BlueZeeKing@users.noreply.github.com>
2022-04-04 09:39:38 -0400

    use fabric api instead of mixin for server start

--- src/main/java/io/icker/factions/FactionsMod.java
+++ src/main/java/io/icker/factions/FactionsMod.java
@@ -7,16 +7,26 @@ import io.icker.factions.config.Config;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import io.icker.factions.command.CommandRegistry;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import io.icker.factions.event.ServerEvents;
 
 public class FactionsMod implements ModInitializer {
 	public static Logger LOGGER = LogManager.getLogger("Factions");
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Initalized Factions Mod for Minecraft v1.17");
+		LOGGER.info("Initalized Factions Mod for Minecraft v1.18");
 		Config.init();
 		CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
 			CommandRegistry.register(dispatcher);
+		});
+
+		ServerLifecycleEvents.SERVER_STARTED.register(server -> {
+			ServerEvents.started(server);
+		});
+
+		ServerLifecycleEvents.SERVER_STOPPED.register(server -> {
+			ServerEvents.stopped(server);
 		});
 	}
 }
