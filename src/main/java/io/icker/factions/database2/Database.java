88613ddd6d33cb95ab1d22349d4244bcb15ff8af
ickerio <harry.ickeringill@gmail.com>
2022-05-10 21:47:02 +1000

    Make Persistent class && setup func

--- src/main/java/io/icker/factions/database2/Database.java
+++ src/main/java/io/icker/factions/database2/Database.java
@@ -13,32 +13,50 @@ import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtIo;
 
 public class Database {
-    public static <T> List<T> load(Class<T> clazz) {
-        String name = clazz.getAnnotation(Compound.class).value();
+    public static final File BASE_PATH = new File("factions");
+    public static final HashMap<Class<?>, HashMap<String, Field>> cache = new HashMap<Class<?>, HashMap<String, Field>>();
+
+    public static <T extends Persistent> void setup(Class<T> clazz) {
+        String name = clazz.getAnnotation(Persistent.Name.class).value();
+        File directory = new File(BASE_PATH + name);
+
+        if (directory.isFile()) {
+            FactionsMod.LOGGER.error("File already exists in database directory path " + directory.toString());
+            return;
+        }
+
+        directory.mkdirs();
 
         HashMap<String, Field> fields = new HashMap<String, Field>();
 
         for (Field field : clazz.getDeclaredFields()) {
             field.setAccessible(true);
-            if (field.isAnnotationPresent(Element.class)) {
-                fields.put(field.getAnnotation(Element.class).value(), field);
+            if (field.isAnnotationPresent(Persistent.Field.class)) {
+                fields.put(field.getAnnotation(Persistent.Field.class).value(), field);
             }
         }
 
-        File directory = new File("factions/" + name); // TODO check if its a file or if its empty and skip
+        cache.put(clazz, fields);
+    }
+
+    public static <T extends Persistent> List<T> load(Class<T> clazz) {
+        String name = clazz.getAnnotation(Persistent.Name.class).value();
+        HashMap<String, Field> fields = cache.get(clazz);
+
+        File directory = new File(BASE_PATH, name);
         File files[] = directory.listFiles();
 
         ArrayList<T> items = new ArrayList<T>();
 
         for (File file : files) {
             try {
-                NbtCompound compound = NbtIo.read(file);
+                NbtCompound compound = NbtIo.readCompressed(file);
                 T item = (T) clazz.getDeclaredConstructor().newInstance();
 
                 for (Map.Entry<String, Field> entry : fields.entrySet()) {
                     String key = entry.getKey();
                     Field field = entry.getValue();
-                    
+
                     Object element = TypeSerializerRegistry.get(field.getType()).readNbt(key, compound);
 
                     field.set(item, element);
@@ -47,7 +65,6 @@ public class Database {
                 items.add(item);
 
             } catch (IOException | ReflectiveOperationException e) {
-                FactionsMod.LOGGER.error(e);
                 FactionsMod.LOGGER.error("Failed to read NBT data");
             }
         }
@@ -55,44 +72,31 @@ public class Database {
         return items;
     }
 
-    public static <T> void save(Class<T> clazz, List<T> items) {
-        String name = clazz.getAnnotation(Compound.class).value();
+    public static <T extends Persistent> void save(Class<T> clazz, List<T> items) {
+        String name = clazz.getAnnotation(Persistent.Name.class).value();
+        HashMap<String, Field> fields = cache.get(clazz);
 
-        HashMap<String, Field> fields = new HashMap<String, Field>();
-        
-        for (Field field : clazz.getDeclaredFields()) {
-            field.setAccessible(true);
-            if (field.isAnnotationPresent(Element.class)) {
-                fields.put(field.getAnnotation(Element.class).value(), field);
-            }
-        }
+        File path = new File(BASE_PATH, name);
 
         for (T item : items) {
             NbtCompound compound = new NbtCompound();
 
-            for (Map.Entry<String, Field> entry : fields.entrySet()) {
-                String key = entry.getKey();
-                Field field = entry.getValue();
+            try {
+                for (Map.Entry<String, Field> entry : fields.entrySet()) {
+                    String key = entry.getKey();
+                    Field field = entry.getValue();
 
-                try {
                     Class<?> type = field.getType();
                     Object data = field.get(item);
-    
-                    //TypeSerializer<?> serializer = TypeSerializerRegistry.get(type);
-                    //serializer.writeNbt(key, compound, type.cast(data));
-    
-                    write(type, data, key, compound);
-                } catch(ReflectiveOperationException e ) {
-                    FactionsMod.LOGGER.error("Failed to write NBT data");
-                }
-            }
 
-            // TODO multiple files by ID && fix this
-            File file = new File("factions/" + name + "/1.dat");
-            try {
-                NbtIo.write(compound, file);
-            } catch (IOException e) {
-                e.printStackTrace();
+                    write(type, data, key, compound);
+                }
+
+                File file = new File(path, item.getKey() + ".dat");
+                NbtIo.writeCompressed(compound, file);
+
+            } catch(IOException | ReflectiveOperationException e ) {
+                FactionsMod.LOGGER.error("Failed to write NBT data");
             }
         }
     }
