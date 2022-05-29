60140b727e33b3bce653b2d5d161613b2d1f20db
BlueZeeKing <github@blueish.dev>
2022-05-28 21:57:57 -0400

    Fix minecraft server mixin

--- src/main/java/io/icker/factions/mixin/MinecraftServerMixin.java
+++ src/main/java/io/icker/factions/mixin/MinecraftServerMixin.java
@@ -1,18 +1,9 @@
 package io.icker.factions.mixin;
 
-import io.icker.factions.FactionsMod;
 import io.icker.factions.event.ChatEvents;
-import io.icker.factions.event.PlayerInteractEvents;
 import net.minecraft.network.ChatDecorator;
-import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.network.ServerPlayNetworkHandler;
-import net.minecraft.text.MutableText;
-import net.minecraft.text.Text;
-import net.minecraft.text.TextContent;
-import net.minecraft.util.Formatting;
 import io.icker.factions.event.ServerEvents;
-import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -27,8 +18,8 @@ public class MinecraftServerMixin {
         cir.setReturnValue((sender, message) -> {
             return CompletableFuture.completedFuture(ChatEvents.handleMessage(sender, message.getString()));
         });
-@Mixin(MinecraftServer.class)
-public class MinecraftServerMixin {
+    }
+
     @Inject(at = @At("HEAD"), method="Lnet/minecraft/server/MinecraftServer;save(ZZZ)Z")
     public void save(boolean suppressLogs, boolean flush, boolean force, CallbackInfoReturnable<Boolean> ci) {
         ServerEvents.save();
