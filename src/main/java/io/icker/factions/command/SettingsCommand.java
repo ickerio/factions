c32181276f82ed5f9bd58aaac75aeb9e91b9ef74
PyPylia <liamhmccracken@gmail.com>
2022-06-10 21:47:59 +1000

    Rewrite one line methods into lambdas

--- src/main/java/io/icker/factions/command/SettingsCommand.java
+++ src/main/java/io/icker/factions/command/SettingsCommand.java
@@ -13,20 +13,8 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Formatting;
 
 public class SettingsCommand implements Command{
-    private int globalChat(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
-        return setChat(context.getSource(), User.ChatMode.GLOBAL);
-    }
-
-    private int factionChat(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
-        return setChat(context.getSource(), User.ChatMode.FACTION);
-    }
-
-    private int focusChat(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
-        return setChat(context.getSource(), User.ChatMode.FOCUS);
-    }
-
-    private int setChat(ServerCommandSource source, User.ChatMode option) throws CommandSyntaxException {
-        ServerPlayerEntity player = source.getPlayer();
+    private int setChat(CommandContext<ServerCommandSource> context, User.ChatMode option) throws CommandSyntaxException {
+        ServerPlayerEntity player = context.getSource().getPlayer();
         User user = User.get(player.getUuid());
         user.chat = option;   
 
@@ -41,26 +29,10 @@ public class SettingsCommand implements Command{
         return 1;
     }
 
-    private int noSounds(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
-        return setSounds(context.getSource(), User.SoundMode.NONE);
-    }
-
-    private int warningSounds(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
-        return setSounds(context.getSource(), User.SoundMode.WARNINGS);
-    }
-
-    private int factionSounds(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
-        return setSounds(context.getSource(), User.SoundMode.FACTION);
-    }
-
-    private int allSounds(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
-        return setSounds(context.getSource(), User.SoundMode.ALL);
-    }
-
-    private int setSounds(ServerCommandSource source, User.SoundMode option) {
-        ServerPlayerEntity player = source.getPlayer();
+    private int setSounds(CommandContext<ServerCommandSource> context, User.SoundMode option) {
+        ServerPlayerEntity player = context.getSource().getPlayer();
         User user = User.get(player.getUuid());
-        user.sounds = option;   
+        user.sounds = option;
 
         new Message("Successfully set your sound preference")
             .filler("·")
@@ -99,9 +71,9 @@ public class SettingsCommand implements Command{
             .then(
                 CommandManager.literal("chat")
                 .requires(Requires.hasPerms("factions.settings.chat", 0))
-                .then(CommandManager.literal("global").executes(this::globalChat))
-                .then(CommandManager.literal("faction").executes(this::factionChat))
-                .then(CommandManager.literal("focus").executes(this::focusChat))
+                .then(CommandManager.literal("global").executes(context -> setChat(context, User.ChatMode.GLOBAL)))
+                .then(CommandManager.literal("faction").executes(context -> setChat(context, User.ChatMode.FACTION)))
+                .then(CommandManager.literal("focus").executes(context -> setChat(context, User.ChatMode.FOCUS)))
             )
             .then(
                 CommandManager.literal("radar")
@@ -111,10 +83,10 @@ public class SettingsCommand implements Command{
             .then(
                 CommandManager.literal("sounds")
                 .requires(Requires.hasPerms("factions.settings.sounds", 0))
-                .then(CommandManager.literal("none").executes(this::noSounds))
-                .then(CommandManager.literal("warnings").executes(this::warningSounds))
-                .then(CommandManager.literal("faction").executes(this::factionSounds))
-                .then(CommandManager.literal("all").executes(this::allSounds))
+                .then(CommandManager.literal("none").executes(context -> setSounds(context, User.SoundMode.NONE)))
+                .then(CommandManager.literal("warnings").executes(context -> setSounds(context, User.SoundMode.WARNINGS)))
+                .then(CommandManager.literal("faction").executes(context -> setSounds(context, User.SoundMode.FACTION)))
+                .then(CommandManager.literal("all").executes(context -> setSounds(context, User.SoundMode.ALL)))
             )
             .build();
     }
