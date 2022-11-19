19b878df46aa36b44cd247d884ef10e66c5a5183
BlueZeeKing <github@blueish.dev>
2022-11-19 15:02:15 -0500

    Remove old log message

--- src/main/java/io/icker/factions/mixin/ServerPlayerInteractionManagerMixin.java
+++ src/main/java/io/icker/factions/mixin/ServerPlayerInteractionManagerMixin.java
@@ -1,6 +1,5 @@
 package io.icker.factions.mixin;
 
-import io.icker.factions.FactionsMod;
 import io.icker.factions.api.events.PlayerEvents;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
@@ -14,8 +13,6 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public class ServerPlayerInteractionManagerMixin {
     @Redirect(method = "interactBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;useOnBlock(Lnet/minecraft/item/ItemUsageContext;)Lnet/minecraft/util/ActionResult;"))
     public ActionResult place(ItemStack instance, ItemUsageContext context) {
-        FactionsMod.LOGGER.info("place attempt");
-
         if (PlayerEvents.PLACE_BLOCK.invoker().onPlaceBlock(context) == ActionResult.FAIL) {
             return ActionResult.FAIL;
         }
