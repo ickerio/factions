ea43d83296191532968495945c0eb0d2cf7f25da
BlueZeeKing <github@blueish.dev>
2023-07-20 22:00:24 -0400

    Add colorless faction name placeholder

--- src/main/java/io/icker/factions/util/PlaceholdersWrapper.java
+++ src/main/java/io/icker/factions/util/PlaceholdersWrapper.java
@@ -16,6 +16,7 @@ import static net.minecraft.util.Formatting.DARK_GRAY;
 
 public class PlaceholdersWrapper {
     public static final Identifier FACTION_NAME_ID = new Identifier(MODID, "name");
+    public static final Identifier FACTION_COLORLESS_NAME_ID = new Identifier(MODID, "colorless_name");
     public static final Identifier FACTION_CHAT_ID = new Identifier(MODID, "chat");
     public static final Identifier FACTION_RANK_ID = new Identifier(MODID, "rank");
     public static final Identifier FACTION_COLOR_ID = new Identifier(MODID, "color");
@@ -26,19 +27,20 @@ public class PlaceholdersWrapper {
     public static final Identifier FACTION_MAX_POWER_ID = new Identifier(MODID, "max_power");
     public static final Identifier FACTION_PLAYER_POWER_ID = new Identifier(MODID, "player_power");
     public static final Identifier FACTION_REQUIRED_POWER_ID = new Identifier(MODID, "required_power");
-    public static final Identifier FACTION_REQUIRED_POWER_FORMATTED_ID = new Identifier(MODID, "required_power_formatted");
+    public static final Identifier FACTION_REQUIRED_POWER_FORMATTED_ID = new Identifier(MODID,
+            "required_power_formatted");
     public static final String NULL_STRING = "N/A";
     public static final Text NULL_TEXT = Text.literal(NULL_STRING).formatted(DARK_GRAY);
 
     public static void init() {
         register(FACTION_NAME_ID, (ctx, argument) -> {
-            if (!ctx.hasPlayer()) return invalid("No Player!");
+            if (!ctx.hasPlayer())
+                return invalid("No Player!");
             assert ctx.player() != null;
 
             Text r = NULL_TEXT;
 
             final var member = User.get(ctx.player().getUuid());
-
 
             final var faction = member.getFaction();
 
@@ -48,8 +50,26 @@ public class PlaceholdersWrapper {
             return value(r);
         });
 
+        register(FACTION_COLORLESS_NAME_ID, (ctx, argument) -> {
+            if (!ctx.hasPlayer())
+                return invalid("No Player!");
+            assert ctx.player() != null;
+
+            Text r = NULL_TEXT;
+
+            final var member = User.get(ctx.player().getUuid());
+
+            final var faction = member.getFaction();
+
+            if (faction != null)
+                r = Text.literal(faction.getName());
+
+            return value(r);
+        });
+
         register(FACTION_CHAT_ID, (ctx, argument) -> {
-            if (!ctx.hasPlayer()) return invalid("No Player!");
+            if (!ctx.hasPlayer())
+                return invalid("No Player!");
             assert ctx.player() != null;
 
             Text r = Text.of("Faction Chat");
@@ -62,7 +82,8 @@ public class PlaceholdersWrapper {
         });
 
         register(FACTION_RANK_ID, (ctx, argument) -> {
-            if (!ctx.hasPlayer()) return invalid("No Player!");
+            if (!ctx.hasPlayer())
+                return invalid("No Player!");
             assert ctx.player() != null;
 
             Text r = NULL_TEXT;
@@ -76,7 +97,8 @@ public class PlaceholdersWrapper {
         });
 
         register(FACTION_COLOR_ID, (ctx, argument) -> {
-            if (!ctx.hasPlayer()) return invalid("No Player!");
+            if (!ctx.hasPlayer())
+                return invalid("No Player!");
             assert ctx.player() != null;
 
             final var member = User.get(ctx.player().getUuid());
@@ -89,7 +111,8 @@ public class PlaceholdersWrapper {
         });
 
         register(FACTION_DESCRIPTION_ID, (ctx, argument) -> {
-            if (!ctx.hasPlayer()) return invalid("No Player!");
+            if (!ctx.hasPlayer())
+                return invalid("No Player!");
             assert ctx.player() != null;
 
             String r = NULL_STRING;
@@ -105,7 +128,8 @@ public class PlaceholdersWrapper {
         });
 
         register(FACTION_STATE_ID, (ctx, argument) -> {
-            if (!ctx.hasPlayer()) return invalid("No Player!");
+            if (!ctx.hasPlayer())
+                return invalid("No Player!");
             assert ctx.player() != null;
 
             String r = NULL_STRING;
@@ -121,7 +145,8 @@ public class PlaceholdersWrapper {
         });
 
         register(FACTION_POWER_ID, (ctx, argument) -> {
-            if (!ctx.hasPlayer()) return invalid("No Player!");
+            if (!ctx.hasPlayer())
+                return invalid("No Player!");
             assert ctx.player() != null;
 
             String r = NULL_STRING;
@@ -137,7 +162,8 @@ public class PlaceholdersWrapper {
         });
 
         register(FACTION_POWER_FORMATTED_ID, (ctx, argument) -> {
-            if (!ctx.hasPlayer()) return invalid("No Player!");
+            if (!ctx.hasPlayer())
+                return invalid("No Player!");
             assert ctx.player() != null;
 
             Text r = NULL_TEXT;
@@ -149,14 +175,16 @@ public class PlaceholdersWrapper {
             if (faction != null) {
                 final int red = mapBoundRange(faction.calculateMaxPower(), 0, 170, 255, faction.getPower());
                 final int green = mapBoundRange(0, faction.calculateMaxPower(), 170, 255, faction.getPower());
-                r = Text.literal("" + faction.getPower()).setStyle(Style.EMPTY.withColor(TextColor.parse("#" + toHexString(red) + toHexString(green) + "AA")));
+                r = Text.literal("" + faction.getPower()).setStyle(
+                        Style.EMPTY.withColor(TextColor.parse("#" + toHexString(red) + toHexString(green) + "AA")));
             }
 
             return value(r);
         });
 
         register(FACTION_MAX_POWER_ID, (ctx, argument) -> {
-            if (!ctx.hasPlayer()) return invalid("No Player!");
+            if (!ctx.hasPlayer())
+                return invalid("No Player!");
             assert ctx.player() != null;
 
             String r = NULL_STRING;
@@ -165,7 +193,8 @@ public class PlaceholdersWrapper {
 
             final var faction = member.getFaction();
 
-            if (faction != null) r = "" + faction.calculateMaxPower();
+            if (faction != null)
+                r = "" + faction.calculateMaxPower();
 
             return value(r);
         });
@@ -176,7 +205,8 @@ public class PlaceholdersWrapper {
         });
 
         register(FACTION_REQUIRED_POWER_ID, (ctx, argument) -> {
-            if (!ctx.hasPlayer()) return invalid("No Player!");
+            if (!ctx.hasPlayer())
+                return invalid("No Player!");
             assert ctx.player() != null;
 
             String r = NULL_STRING;
@@ -192,7 +222,8 @@ public class PlaceholdersWrapper {
         });
 
         register(FACTION_REQUIRED_POWER_FORMATTED_ID, (ctx, argument) -> {
-            if (!ctx.hasPlayer()) return invalid("No Player!");
+            if (!ctx.hasPlayer())
+                return invalid("No Player!");
             assert ctx.player() != null;
 
             Text r = NULL_TEXT;
@@ -201,11 +232,11 @@ public class PlaceholdersWrapper {
 
             final var faction = member.getFaction();
 
-
             if (faction != null) {
                 final int reqPower = faction.getClaims().size() * FactionsMod.CONFIG.POWER.CLAIM_WEIGHT;
                 final int red = mapBoundRange(0, faction.getPower(), 85, 255, reqPower);
-                r = Text.literal("" + reqPower).setStyle(Style.EMPTY.withColor(TextColor.parse("#" + toHexString(red) + "5555")));
+                r = Text.literal("" + reqPower)
+                        .setStyle(Style.EMPTY.withColor(TextColor.parse("#" + toHexString(red) + "5555")));
             }
 
             return value(r);
